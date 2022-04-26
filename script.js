a03566c6657b0206e00f5d312a7c280cac0a9d44
Kerwin <tkoleo84119@gmail.com>
2022-04-26 20:50:38 +0800

    feat: add JSDoc

--- script.js
+++ script.js
@@ -4,17 +4,33 @@ const controls = document.querySelector(".controls");
 const root = document.documentElement;
 
 const inputType = {
+  /**
+   * This function is change css variable(spacing).
+   * @param {string} value - The value of spacing input.
+   */
   spacing(value) {
     root.style.setProperty("--spacing", `${value}px`);
   },
+  /**
+   * This function is change css variable(blur).
+   * @param {string} value - The value of blur input.
+   */
   blur(value) {
     root.style.setProperty("--blur", `${value}px`);
   },
+  /**
+   * This function is change css variable(base-color).
+   * @param {string} value - The name of color.
+   */
   base(value) {
     root.style.setProperty("--base-color", `${value}`);
   },
 };
 
+/**
+ * This function is change style base on input.
+ * @param {object} e - The event object.
+ */
 const changeStyles = function (e) {
   const { name, value } = e.target;
   inputType[name](value);
